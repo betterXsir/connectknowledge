a9e6461161eeeaf335eae6971b90c8ed2df2d949
betterXsir <foundbanana@icloud.com>
2017-08-14 23:22:38 +0800

    add signin.html

--- src/main/java/com/hzh/snails/connectknowledge/web/GreetingController.java
+++ src/main/java/com/hzh/snails/connectknowledge/web/GreetingController.java
@@ -9,13 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class GreetingController {
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World")
-                                       String name, Model model){
-        User user = new User();
-        user.setId(1);
-        user.setName(name);
-        model.addAttribute("name",name);
-        return "example2";
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+    @RequestMapping("/signin")
+    public String signin(){
+        return "signin";
     }
 }
