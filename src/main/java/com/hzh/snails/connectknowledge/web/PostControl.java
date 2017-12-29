1e9a0bc9a3ab0437fb4b8e9abb4187ec8324bf4f
huzhenhua <lhzgege5277@163.com>
2017-12-29 18:19:14 +0800

    2017-12-29

--- src/main/java/com/hzh/snails/connectknowledge/web/PostControl.java
+++ src/main/java/com/hzh/snails/connectknowledge/web/PostControl.java
@@ -8,13 +8,17 @@ import com.hzh.snails.connectknowledge.common.ServerResponse;
 import com.hzh.snails.connectknowledge.domain.PostWithBLOBs;
 import com.hzh.snails.connectknowledge.domain.User;
 import com.hzh.snails.connectknowledge.service.PostServiceImpl;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpSession;
@@ -28,6 +32,29 @@ import java.io.IOException;
 public class PostControl {
     @Autowired
     private PostServiceImpl postService;
+
+    @RequestMapping(value="falconDingDing")
+    @ResponseBody
+    public ServerResponse falconDingDing(@RequestParam String content){
+        String WEBHOOK_TOKEN = "https://oapi.dingtalk.com/robot/send?access_token=40042c2ab93ef6bd713501f022f90f20d726f05a25452657be97b52f8a1d586c";
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(WEBHOOK_TOKEN);
+        httpPost.addHeader("Content-Type", "application/json; charset=utf-8");
+        String textMsg = "{\"msgtype\":\"text\",\"text\":{\"content\":\"test\"}}";
+        StringEntity se = new StringEntity(textMsg, "utf-8");
+        httpPost.setEntity(se);
+
+        try {
+            HttpResponse response = httpClient.execute(httpPost);
+            if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
+                String result = EntityUtils.toString(response.getEntity(), "utf-8");
+                return ServerResponse.createBySuccessMessage(result);
+            }
+            return ServerResponse.createByErrorMessage("Connect Error");
+        }catch (IOException var){
+            return ServerResponse.createByErrorMessage("IO Error");
+        }
+    }
 
     @RequestMapping(value="writer")
     public String getWriter(){
